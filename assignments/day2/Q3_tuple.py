ccbbe29eb3bca60b3c602f415fd8eb1c2f5ca38b
mohitarya <mohitarya8@gmail.com>
2016-05-25 22:21:08 +0530

    Added Q3

---
+++ assignments/day2/Q3_tuple.py
@@ -0,0 +1,12 @@
+""" Day2, Program-3
+    Create a simple tuple. Can you add mutable
+    objects like list to tuple? Show with example
+"""
+
+test_tuple = (1, 2, 3, 4, "Hello World")
+
+print test_tuple
+
+test_tuple.insert(2, 123)
+
+print test_tuple
