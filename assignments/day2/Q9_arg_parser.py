97c590e87cdde38c127913f0ef48c6d97bbc6641
mohitarya <mohitarya8@gmail.com>
2016-05-26 01:22:08 +0530

    Added Q9

---
+++ assignments/day2/Q9_arg_parser.py
@@ -0,0 +1,15 @@
+""" Day2, Prorgam-9
+    write a program which accept two positional
+    parameter (Name and age) and parse that Name
+    is string and age is int using argpasre and
+    display the help message for each parameter.
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("Name", help="Input the name")
+parser.add_argument("age", help="Input the age")
+args = parser.parse_args()
+print(args.Name)
+print(args.age)
