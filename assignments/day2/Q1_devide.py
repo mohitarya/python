1065c9468a848afaaa8d21a293aa806e23473df9
mohitarya <mohitarya8@gmail.com>
2016-05-25 21:59:58 +0530

    addded Q1

---
+++ assignments/day2/Q1_devide.py
@@ -0,0 +1,12 @@
+""" Day2, Program-1
+    1) Write a small python program which includes:
+        i) Two variables say a and b having values 10 and 20 respectively
+        ii) Divide variable b by 10 and print result. Use shorthand notation
+"""
+#initializing Variables
+a = 10
+b = 20
+
+#divide using short hand notation
+b /= a
+print b 
