af57b6b3e31415ddc33f4e7d5491ab6f995709aa
mohitarya <mohitarya8@gmail.com>
2016-05-25 22:36:27 +0530

    Added Q4

---
+++ assignments/day2/Q4_mapping_of_machine.py
@@ -0,0 +1,17 @@
+""" Day2, Program-3
+    You want to store mapping of machine name and
+    its IP address. Identify appropriate data type
+    to store this info and print it.
+    (Note: Consider 5 entries only) 
+
+"""
+
+#Choosing dictionary to storing the mapping machine
+
+machine_info_dict = {"m1": "10.10.10.1", \
+                     "m2": "10.10.10.2", \
+                     "m3": "10.101.10.3",\
+                     "m4": "10.101.10.4", \
+                     "m5": "10.10.10.5" }
+
+print machine_info_dict
